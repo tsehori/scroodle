e245da03d0be1bbed4bb142de75977ddeef7cf9c
Gilad <g.tsehori@gmail.com>
2018-08-25 03:58:45 +0300

    Created courses_dict
    Dictionary courses_dict consists of course names with corresponding course codes.
    Initialized function add_course_new_info will take the new dataframe, and add the course information from the full activities list, at url http://moodle.idc.ac.il/2018/blocks/idc_news/full_list.php?courseid=<course_code>.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
-from robobrowser import RoboBrowser as rb
+import pandas as pd
+from robobrowser import RoboBrowser
 import my_creds
 
 preferred_lang = 'en'  # Your preferred language
 
+
 def init_connection():
-    browser = rb()
     browser.open('https://my.idc.ac.il/')
     login_form = browser.get_form(id='auth_form')
     login_form['username'].value = my_creds.USERNAME
@@ -14,22 +13,39 @@ def init_connection():
     browser.submit_form(login_form)
 
     semester_dict = {'Semester 1': '16', 'Semester 2': '95', 'Semester 3': '134'}
-    current_semester = 'Semester 2'
+    current_semester = 'Semester 1'
 
     browser.open('http://moodle.idc.ac.il/2018/my/index.php?lang={}'.format(preferred_lang))
     semester_form = browser.get_forms()[1]
     semester_form['coc-category'].value = semester_dict['{}'.format(current_semester)]
-    all_courses_links = [BeautifulSoup(str(div_class)).a for div_class in
-                         browser.select(".coc-category-{}".format(semester_dict[current_semester]))]
-
-# all_courses_links = [link.find_all('a')
-#                      for link in browser.select(".coc-category-{}".format(semester_dict[current_semester]))]
+    # all_courses_links = [BeautifulSoup(str(div_class)).a for div_class in
+    #                      browser.select(".coc-category-{}".format(semester_dict[current_semester]))]
+    # all_courses_links = [link.find_all('a')
+    #                       for link in browser.select(".coc-category-{}".
+    #                       format(semester_dict[current_semester]))]
 
     all_courses_links = [url.get('href') for url in
                          [link.find('a') for link in browser.select(".coc-category-{}".format(
                          semester_dict[current_semester]))]]
 
-    all_courses_codes = [code.split('=')[1] for code in all_courses_links]
-    print(all_courses_codes)
+    return [code.split('=')[1] for code in all_courses_links]
 
-init_connection()
+
+def get_course_name(code):
+    browser.open('http://moodle.idc.ac.il/2018/course/view.php?id={}'.format(code))
+    return browser.select('.page-header-headings')[0].h1.string
+
+
+def add_course_new_info(df):
+    pass
+
+
+browser = RoboBrowser()
+df = pd.DataFrame(index=['Time'], columns=['Course', 'Text', 'Activity', 'Action'])
+codes = init_connection()
+courses_dict = dict()
+for code in codes:
+    courses_dict['{}'.format(get_course_name(code))] = code
+
+for course in courses_dict:
+    add_course_new_info(df)
