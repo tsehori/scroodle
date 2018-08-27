5e81c52c0250981810c9cb911bf5569d210b7083
Gilad <g.tsehori@gmail.com>
2018-08-27 18:02:28 +0300

    Reformatting and added config.py
    config.py contains all relevant configurations settings for the functions in main.py.

---
+++ config.py
@@ -0,0 +1,5 @@
+SEMESTER_DICT = {'Semester 1': '16',
+                 'Semester 2': '95',
+                 'Semester 3': '134'}
+
+MY_IDC_HOME = 'https://my.idc.ac.il/'
--- main.py
+++ main.py
@@ -3,39 +3,39 @@ from datetime import datetime
 from robobrowser import RoboBrowser
 from tabulate import tabulate
 import my_creds
-from config import determine_semester
+import config
 
-preferred_lang = 'en'  # 'en' or 'he'
-current_semester = 'Semester 2'  # 'Semester 1', 'Semester 2' or 'Semester 3'
-# current_semester = determine_semester(datetime.now())
+PREFERRED_LANG = 'en'  # 'en' or 'he'
+CURRENT_SEMESTER = 'Semester 2'  # 'Semester 1', 'Semester 2' or 'Semester 3'
 
 
-def init_connection():
+def init_connection(browser):
     """Initializes connection with Moodle website and
-       log ins with user credentials"""
-    browser.open('https://my.idc.ac.il/')
+       log ins with user credentials
+    :param: browser: RoboBrowser object
+    """
+    browser.open(config.MY_IDC_HOME)
     login_form = browser.get_form(id='auth_form')
     login_form['username'].value = my_creds.USERNAME
     login_form['password'].value = my_creds.PASSWORD
     browser.submit_form(login_form)
 
 
-def get_courses_codes():
+def get_courses_codes(browser):
     """
+    :param: browser: RoboBrowser object
     :return: Courses codes for current semester
     """
-    semester_dict = {'Semester 1': '16',
-                     'Semester 2': '95',
-                     'Semester 3': '134'}
+
     browser.open('http://moodle.idc.ac.il/2018/my/index.php?lang={}'
-                 .format(preferred_lang))
+                 .format(PREFERRED_LANG))
     semester_form = browser.get_forms()[1]
-    semester_form['coc-category'].value = semester_dict['{}'
-        .format(current_semester)]
+    semester_form['coc-category'].value = \
+        config.SEMESTER_DICT['{}'.format(CURRENT_SEMESTER)]
     all_courses_links = [url.get('href') for url in
                          [link.find('a') for link in browser.select(
                              ".coc-category-{}".format(
-                                 semester_dict[current_semester]))]]
+                                 config.SEMESTER_DICT[CURRENT_SEMESTER]))]]
     return [code.split('=')[1] for code in all_courses_links]
 
 
@@ -49,9 +49,9 @@ def get_course_name(code):
     return browser.select('.page-header-headings')[0].h1.string
 
 
-def add_course_new_info(df, course_name, num_last_items=3):
+def add_course_new_info(main_df, course_name, num_last_items=3):
     """
-    :param df: Either an empty dataframe or a dataframe containing previous
+    :param main_df: Either an empty dataframe or a dataframe containing previous
                courses information
     :param course_name: String
     :param num_last_items: Number of last items from each course
@@ -59,26 +59,25 @@ def add_course_new_info(df, course_name, num_last_items=3):
     """
     browser.open('http://moodle.idc.ac.il/{}/blocks/idc_news/'
                  'full_list.php?courseid={}'.format(
-                  datetime.now().year, courses_dict[course_name]
-                 ))
+                  datetime.now().year, courses_dict[course_name]))
     course_info = pd.read_html(str(browser.find('table')))[0]\
         .head(num_last_items)
     course_info['Course Name'] = course_name
     course_info['View'] = [item.get('href')
                            for item in browser.find('table')
                                .find_all('a')][:num_last_items]
-    if len(df.index) == 0:  # If the dataframe is empty, initialize it
-        df = course_info
+    if len(main_df.index) == 0:  # If the dataframe is empty, initialize it
+        main_df = course_info
     else:
-        df = df.append(course_info, ignore_index=True)
-    return df
+        main_df = main_df.append(course_info, ignore_index=True)
+    return main_df
 
 
 if __name__ == '__main__':
     df = pd.DataFrame()
-    browser = RoboBrowser()
-    init_connection()
-    course_codes = get_courses_codes()
+    browser = RoboBrowser(parser='html.parser')
+    init_connection(browser=browser)
+    course_codes = get_courses_codes(browser=browser)
     courses_dict = dict()
 
     for code in course_codes:
