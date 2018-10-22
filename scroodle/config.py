4c16b5c270f9d0a07af6a0db13f9b83f0593418f
Gilad <g.tsehori@gmail.com>
2018-10-22 16:20:26 +0300

    Merged changes

--- scroodle/config.py
+++ scroodle/config.py
@@ -9,11 +9,13 @@ SEMESTER_DICT = {'1': '16',   # Semester 1
 
 MY_IDC_HOME = 'https://my.idc.ac.il/'
 
-MOODLE_MAIN_PAGE = 'http://moodle.idc.ac.il/{}/my/' \
+MOODLE_WEBSITE = 'http://moodle.idc.ac.il/'
+
+MOODLE_MAIN_PAGE = MOODLE_WEBSITE + '{}/my/' \
                    'index.php?lang={}'  # Year, language
 
-COURSE_MAIN_PAGE = 'http://moodle.idc.ac.il/{}/course/' \
+COURSE_MAIN_PAGE = MOODLE_WEBSITE + '{}/course/' \
                    'view.php?id={}'  # Year, Course code
 
-COURSE_NEW_ITEMS_PAGE = 'http://moodle.idc.ac.il/{}/blocks/' \
+COURSE_NEW_ITEMS_PAGE = MOODLE_WEBSITE + '{}/blocks/' \
                         'idc_news/full_list.php?courseid={}'
